37c28b879c4fa85bde6fb2457f93f4c44ac1c1b0
Mark Apinis <mapinis2019@gmail.com>
2017-04-15 16:50:52 -0400

    A bit more working animation

--- experiments/roll.js
+++ experiments/roll.js
@@ -1,13 +1,13 @@
 var cen = 400;
 var colorRoller = 'blue';
 var colorCircle = 'red';
-var sides = 2;
+var sides = 8;
 var radSmall = 195;
 var radBig = 390;
 
 new Circle(cen, cen, radBig).stroke(colorCircle, 3).addTo(stage);
 
-/*var roller = new Path().stroke(colorRoller, 3).moveTo(cen, cen);
+var roller = new Path().stroke(colorRoller, 3).moveTo(cen, cen);
 	for(var k = 1; k <= sides; k++){
 		roller.lineTo(radSmall * Math.cos((k * 2 * Math.PI) / sides) + cen - radSmall,
 					 radSmall * Math.sin((k * 2 * Math.PI) / sides) + cen);
@@ -16,24 +16,13 @@ new Circle(cen, cen, radBig).stroke(colorCircle, 3).addTo(stage);
 
 roller.addTo(stage);
 
-roller.attr({ rotation: 0, origin: { x: cen, y: cen } });
+roller.attr({ rotation: 0, origin: { x: cen - radSmall, y: cen } });
 roller.animate(new KeyframeAnimation('5s', {
 	from: { rotation: 0 },
 	to: { rotation: 2 * Math.PI }
+}, { repeat: Infinity }));
+/*roller.animate(new KeyframeAnimation('5s', {
+	from: { x: 0, y: 0, origin: { x: 0, y: 0 } },
+	'50%': { x: cen, y: cen, origin: { x: cen - radSmall, y: cen } },
+	to: { x: 0, y: 0, origin: { x: 0, y: 0 } }
 }, { repeat: Infinity }));*/
-
-var points = [];
-for(var k = 1; k <= sides; k++){
-		points.push([radSmall * Math.cos((k * 2 * Math.PI) / sides) + cen - radSmall,
-					 radSmall * Math.sin((k * 2 * Math.PI) / sides) + cen]);
-	}
-
-var circles = [];
-points.forEach(function(point) {
-	circles.push(new Circle(point[0], point[1], 5).fill(colorRoller).addTo(stage))
-}, this);
-
-circles.forEach(function(circle) {
-	circle.animate()
-}, this);
-
